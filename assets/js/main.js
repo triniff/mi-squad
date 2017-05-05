b50988e0c0463738edbbbc5e0ebd0555ae82d73e
“Trinidad <tfuentesfuentealba@gmail.com>
2017-05-04 22:29:56 -0300

    creacion lista disfunsional

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,23 @@
 function Squad(nombre,edad,hobbies){
 	this.nombre = nombre
 	this.edad = edad
+	this.hobbies = hobbies;
 
+	this.lista = function(){
+			var final;
+			for(var i = 0; i < this.hobbies.length; i++){
+				final += "<li>" + this.hobbies[i] + "</li>";
+			}
+			return final
+
+	}
 	this.mostrar = function (){
-		return ("Nombre: " + this.nombre + "<br>" + "Edad: " + this.edad + "<br>" +  "Hobbies: " + this.hobbies)
+		return ("<b>" + "Nombre: " + "</b>" + this.nombre + "<br>" + "<b>" + "Edad: " +  "</b>"  + this.edad + "<br>" + "<b>" +  "Hobbies: " + "</b>" + "<ul>" + this.lista() +"</ul>")
 	}
 }
 
 
-var valeLopez = new Squad ("valentina lopez", 25, ["Tarot", "Idiomas", "Películas mudas"]);
+var valeLopez = new Squad ("Valentina Lopez", 25, ["Tarot", "Idiomas", "Películas mudas"]);
 var trini = new Squad("Trinidad Fuentes", 26, ["Video juegos", "PugLover", "Dormir"]);
 var diana = new Squad("Diana Monsalve", 29,  ["Hacer scratch", "Música", "Negocios"]);
 var dani = new Squad("Daniela Sanchez", 24,["Escuchar música", "Cantar", "Ver series"]);
@@ -19,3 +28,11 @@ var kari = new Squad("Karina Palacios", 24, ["Ver series", "Compartir con amigos
 var gilia = new Squad ("Gilia Gongora", 28, ["La destrución", "Satán", "Infierno"]);
 
 datosValeL.innerHTML = valeLopez.mostrar();
+datosTrini.innerHTML = trini.mostrar();
+datosDiana.innerHTML = diana.mostrar();
+datosDani.innerHTML = dani.mostrar();
+datosPez.innerHTML = pez.mostrar();
+datosMari.innerHTML = mari.mostrar();
+datosValeT.innerHTML = valeT.mostrar();
+datosKari.innerHTML = kari.mostrar();
+datosGilia.innerHTML = gilia.mostrar();
